019b10ab9abec5d0e49052446d81e548903043be
navneetnavs <navneetkumar2004@icloud.com>
2023-11-12 07:52:50 +0530

    Recursion

---
+++ Recursion/Number/number.java
@@ -0,0 +1,17 @@
+package Recursion.Number;
+
+public class number {
+public static void printDec(int n){
+    if(n==1){
+        System.out.println(1);
+        return;
+    }
+    System.out.println(n+" ");
+    printDec(n-1);
+}
+public static void main (String args[]){
+    int n=10;
+    printDec(n);
+ }  
+}
+
